3ec2769e383549fc8c89876fca9a227ab6475362
inboxtoshashi.github.io <inboxtoshashi@gmail.com>
2025-05-22 23:31:51 +0530

    fixed ssh script

--- ssh_url_ec2/ssh.sh
+++ ssh_url_ec2/ssh.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
 # Navigate to the Terraform directory
-cd /path/to/your/terraform-repo
+cd /var/lib/jenkins/workspace/url-infra/
 
 # Get the EC2 public IP from Terraform output
-EC2_IP=$(terraform output -raw ec2_public_ip)
+EC2_IP=$(terraform output -raw public_ip)
 
 echo "✅ EC2 Public IP is $EC2_IP"
 
 # SSH into the EC2 using the IP
-ssh -i "your-key.pem" ec2-user@$EC2_IP << EOF
+ssh -i "url_app.pem" \
+    -o StrictHostKeyChecking=no \
+    -o UserKnownHostsFile=/dev/null \
+    ubuntu@$EC2_IP << EOF
     echo "🔧 Installing requirements..."
     bash -s < install_requirements/download_docker.sh
 
     echo "🚀 Deploying app..."
     bash -s < deploy_app/deploy_app.sh
 EOF
+
+
+
